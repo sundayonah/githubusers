5683d9104b376df672500a2948838d04d49dbc30
sundayonah <sundayonah94@gmail.com>
2024-10-10 11:48:02 +0100

    delete some files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import Dashboard from "./Dashboard";
-import Login from "./Login";
-import AuthWrapper from "./AuthWrapper";
-import PrivateRoute from "./PrivateRoute";
-import Error from "./Error";
+import Dashboard from './Dashboard';
+import Login from './Login';
+import Error from './Error';
 
-export { Dashboard, Login, AuthWrapper, PrivateRoute, Error };
+export { Dashboard, Login, Error };
